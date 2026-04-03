eb79b7b5ca261e3405f8deb1e9a2e60e99201c96
Dre Devaere <dre.devaere@ugent.be>
2026-04-03 17:03:16 +0200

    speed motor verandert

--- tests/code_1_motor_draait_en_kalibreert/code_1_motor_draait_en_kalibreert.ino
+++ tests/code_1_motor_draait_en_kalibreert/code_1_motor_draait_en_kalibreert.ino
@@ -5,7 +5,7 @@ const int stepsPerRevolution = 2048;
 Stepper myStepper(stepsPerRevolution, 8, 10, 9, 11);
 
 void setup() {
-  myStepper.setSpeed(15);
+  myStepper.setSpeed(10);
   Serial.begin(9600);
   
   Serial.println("Systeem start op... Kalibreren.");
