5c273e03f0b9890fde515253ae737c6baf0053d4
Dre Devaere <dre.devaere@ugent.be>
2026-04-12 17:06:11 +0200

    Nieuwe code met led erbij

---
+++ tests/code_3_Led_erbij/code_3_Led_erbij.ino
@@ -0,0 +1,64 @@
+#include <Stepper.h>
+#include <DHT.h>
+
+#define DHTPIN A3
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
+const int redPin = 5;
+const int greenPin = 6;
+const int stepsPerRevolution = 2048;
+Stepper myStepper(stepsPerRevolution, 8, 10, 9, 11);
+
+// Positie 0 = startpunt, Positie 1 = andere kant
+int Positie = 0;
+
+void setup() {
+  myStepper.setSpeed(15);
+  Serial.begin(9600);
+  dht.begin();
+  
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  digitalWrite(redPin, LOW);
+  digitalWrite(greenPin, HIGH);
+  
+  Serial.println("Systeem start op... Kalibreren.");
+  // De motor draait 800 stappen
+  myStepper.step(800);
+  Serial.println("Kalibratie voltooid.");
+  
+  Positie = 0;
+  delay(2000);
+}
+
+void loop() {
+  float vochtigheid = dht.readHumidity();
+
+  Serial.print("Vochtigheid: ");
+  Serial.print(vochtigheid);
+  Serial.println("%");
+
+  // Draai naar de andere kant (-2048)
+  // Alleen als vochtigheid tussen 40 en 60 is en positie staat nog op 0
+  if (vochtigheid >= 40 && vochtigheid <= 60 && Positie == 0) {
+    Serial.println("Vochtigheid optimaal (40-60%)");
+    myStepper.step(-500);
+    digitalWrite(redPin, HIGH);
+    digitalWrite(greenPin, LOW);
+    Positie = 1;
+  } 
+
+  // Draai terug naar 0 (+2048)
+  // Alleen als de motor aan de andere kant staat (Positie 1)
+  // En de vochtigheid is buiten het bereik (lager dan 40 OF hoger dan 60)
+  else if ((vochtigheid < 40 || vochtigheid > 60) && Positie == 1) {
+    Serial.println("Vochtigheid buiten bereik!");
+    myStepper.step(800);
+    digitalWrite(redPin, LOW); // Wordt rood
+    digitalWrite(greenPin, HIGH);
+    Positie = 0;
+  }
+
+  delay(2000);
+}
